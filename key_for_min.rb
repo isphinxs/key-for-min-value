0c9a6549c0f4fdf29e8a39be26be5b8a37708f27
Samantha Ostrowski <isphinxs@hotmail.com>
2020-11-16 21:39:27 -0500

    commit tree 0203bf7af1d76394c157bc46348ac6250cf76976

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,24 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    if name_hash.size() == 0
+        nil
+    else 
+        min_key = ""
+        min_value = 0
+        first_key = true 
+        name_hash.each do |key, value|
+            if first_key
+                min_key = key
+                min_value = value
+                first_key = false
+            else 
+                if value < min_value 
+                    min_value = value
+                    min_key = key
+                end 
+            end 
+        end
+        min_key
+    end 
 end
